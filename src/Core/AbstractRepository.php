af930f009e7fe151b4dbb835d469a4e8bc59ff45
DerZander <timo.zander@outlook.com>
2020-12-13 00:49:47 +0100

    (feat): create the AbstractController.php, AbstractModel.php, AbstractRepository.php

---
+++ src/Core/AbstractRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Core;
+
+use PDO;
+
+abstract class AbstractRepository
+{
+    protected $pdo;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    abstract public function getTableName();
+    abstract public function getModelName();
+
+
+    function fetchAll()
+    {
+        $table = $this->getTableName();
+        $model = $this->getModelName();
+        $stmt = $this->pdo->query("SELECT * FROM `$table`");
+        return $stmt->fetchAll(PDO::FETCH_CLASS, $model);
+    }
+
+    function fetchOne($id)
+    {
+        $table = $this->getTableName();
+        $model = $this->getModelName();
+        $stmt = $this->pdo->prepare("SELECT * FROM $table WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $model);
+        return $stmt->fetch(PDO::FETCH_CLASS);
+    }
+}
