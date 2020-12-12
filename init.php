040cc5c797f027a9b242858adc37ae6b07f0c7ba
DerZander <timo.zander@outlook.com>
2020-12-13 00:41:41 +0100

    (feat): create proctec function for db-entries

--- init.php
+++ init.php
@@ -3,6 +3,12 @@
 require __DIR__ . "/autoload.php";
 require __DIR__ . "/db.php";
 
+//Schutz vor Scripts oder schädlichen Angriffen durch neue Kommentare
+function e($string)
+{
+    return htmlentities($string, ENT_QUOTES, 'UTF-8');
+}
+
 $container = new App\Core\Container();
 
 ?>
