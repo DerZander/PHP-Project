d9a0ff8e65ec01862350269d595ea2410cdeb669
DerZander <timo.zander@outlook.com>
2020-12-13 00:38:20 +0100

    (refactor): create a simple router like django for this project

--- public/urls.php
+++ public/urls.php
@@ -1,3 +1,24 @@
-<?php
+<?php /** @noinspection PhpUndefinedVariableInspection */
+require __DIR__ . "/../init.php";
 
+$pathInfo = $_SERVER['PATH_INFO'];
+
+$routes = [
+    '/index' => [
+        'controller' => 'productsController',
+        'method' => 'index',
+    ],
+    '/detail' => [
+        'controller' => 'productsController',
+        'method' => 'detail',
+    ]
+];
+
+if(isset($routes[$pathInfo])){
+    $route = $routes[$pathInfo];
+    $controller = $container->make($route['controller']);
+    $method = $route['method'];
+
+    $controller->$method();
+}
 ?>
