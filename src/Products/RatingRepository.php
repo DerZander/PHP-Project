8525136aa8a91bf21614ed0834ffdbb9b78f59f3
DerZander <timo.zander@outlook.com>
2020-12-13 00:50:47 +0100

    (feat): create the fetchbyProduct  function and the create function

---
+++ src/Products/RatingRepository.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Products;
+
+use PDO;
+use App\Core\AbstractRepository;
+
+class RatingRepository extends AbstractRepository
+{
+    public function getTableName(){
+        return "ratings";
+    }
+
+    public function getModelName(){
+        return "App\\Products\\RatingModel";
+    }
+
+    public function create($product_id, $content, $stars){
+        $table = $this->getTableName();
+        $stmt = $this->pdo->prepare("INSERT INTO `$table` (`date`, `rating`, `stars`, `product_id`) VALUES (current_timestamp(), :content, :stars, :product_id);");
+        $stmt->execute([
+            'content' => $content,
+            'product_id' => $product_id,
+            'stars' => $stars,
+        ]);
+    }
+
+
+    public function fetchAllByProduct($id){
+        $table = $this->getTableName();
+        $model = $this->getModelName();
+
+        $stmt = $this->pdo->prepare("SELECT * FROM `$table` WHERE `product_id` = :id");
+        $stmt->execute(['id' => $id]);
+
+        $ratings = $stmt->fetchAll(PDO::FETCH_CLASS, $model);
+        return $ratings;
+    }
+}
+?>
