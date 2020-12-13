d7c6ceed3ecdd9235de18897afd5149ea26f3fce
DerZander <timo.zander@outlook.com>
2020-12-13 16:27:16 +0100

    (feat): create a list for admins

---
+++ views/products/admin/list.php
@@ -0,0 +1,43 @@
+<?php include __DIR__ . "/../../layouts/header.php" ?>
+    <div class="card text-dark bg-light m-3 ">
+        <div class="card-header">
+            <div class="row">
+                <div class="col">
+                    <h1 class="m-0">
+                        Produkte
+                    </h1>
+                </div>
+                <div class="col mt-2">
+                    <div class="d-flex justify-content-end ">
+                        <a class="btn btn-success" href="" >neues Produkt</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <table class="card-body table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Beschreibung</th>
+                <th scope="col">Preis</th>
+                <th scope="col">Option</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($products AS $product): ?>
+                <tr>
+                    <td><?php echo $product['id']?></td>
+                    <td><?php echo $product['name']?></td>
+                    <td><?php echo $product['description']?></td>
+                    <td><?php echo $product['price']?></td>
+                    <td class="col-2">
+                        <a class="btn btn-warning">Bearbeiten</a>
+                        <a class="btn btn-danger">Löschen</a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+    </div>
+<?php include __DIR__ . "/../../layouts/footer.php" ?>
