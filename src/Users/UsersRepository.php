64018257c478927a6529f8af6981214400c9db50
DerZander <timo.zander@outlook.com>
2020-12-13 15:32:03 +0100

    (feat): create UsersRepository.php

--- src/Users/UsersRepository.php
+++ src/Users/UsersRepository.php
@@ -4,7 +4,25 @@
 namespace App\Users;
 
 
-class UsersRepository
-{
+use App\Core\AbstractRepository;
+use PDO;
 
+class UsersRepository extends AbstractRepository
+{
+    public function getTableName(){
+        return "users";
+    }
+
+    public function getModelName(){
+        return "App\\Users\\UserModel";
+    }
+
+    public function findByUsername($username){
+        $table = $this->getTableName();
+        $model = $this->getModelName();
+        $stmt = $this->pdo->prepare("SELECT * FROM $table WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $model);
+        return $stmt->fetch(PDO::FETCH_CLASS);
+    }
 }
