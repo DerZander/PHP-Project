d9b31519d13f86d5b567941ed11c6887b3b10883
DerZander <timo.zander@outlook.com>
2020-12-13 15:35:41 +0100

    (feat): add filter by Category function

--- src/Products/ProductsRepository.php
+++ src/Products/ProductsRepository.php
@@ -2,6 +2,7 @@
 namespace App\Products;
 
 use App\Core\AbstractRepository;
+use PDO;
 
 class ProductsRepository extends AbstractRepository
 {
@@ -12,17 +13,21 @@ class ProductsRepository extends AbstractRepository
     public function getModelName(){
         return "App\\Products\\ProductModel";
     }
-    /*
     function fetchFilteredProducts($category)
     {
-        $stmtCategory = $this->pdo->prepare("SELECT id FROM categories WHERE name = :category");
-        $stmtCategory->execute(['category' => $category]);
-        $stmtCategory->setFetchMode(PDO::FETCH_CLASS, "App\\Categories\\CategoryModel");
-        $categoryId = $stmtCategory->fetch(PDO::FETCH_CLASS)->id;
-        $stmt = $this->pdo->query("SELECT * FROM products WHERE category = {$categoryId}");
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "App\\products\\ProductModel");
-        return $stmt->fetchAll(PDO::FETCH_CLASS, "App\\products\\ProductModel");
+        $table = $this->getTableName();
+        $model = $this->getModelName();
 
-    }*/
+        $categoryStmt = $this->pdo->prepare("SELECT * FROM `categories` WHERE `name` = :category");
+        $categoryStmt->execute(['category' => $category]);
+        $categoryStmt->setFetchMode(PDO::FETCH_CLASS, "App\\Products\\CategoryModel");
+        $categoryId = $categoryStmt->fetch(PDO::FETCH_CLASS);
+        $stmt = $this->pdo->prepare("SELECT * FROM `$table` WHERE `category_id` = :categoryId");
+        $stmt->execute(['categoryId' => $categoryId['id']]);
+
+        $products = $stmt->fetchAll(PDO::FETCH_CLASS, $model);
+        return $products;
+    }
+
 }
 ?>
