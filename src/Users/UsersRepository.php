73803e153eb2ea3ca2dc20d50d5f8de6543a67da
DerZander <timo.zander@outlook.com>
2020-12-14 20:21:35 +0100

    (refactor): change register into create and update function for user

--- src/Users/UsersRepository.php
+++ src/Users/UsersRepository.php
@@ -17,16 +17,7 @@ class UsersRepository extends AbstractRepository
         return "App\\Users\\UserModel";
     }
 
-    public function findByUsername($username){
-        $table = $this->getTableName();
-        $model = $this->getModelName();
-        $stmt = $this->pdo->prepare("SELECT * FROM $table WHERE username = :username");
-        $stmt->execute(['username' => $username]);
-        $stmt->setFetchMode(PDO::FETCH_CLASS, $model);
-        return $stmt->fetch(PDO::FETCH_CLASS);
-    }
-
-    public function register($username, $email, $password){
+    public function create($username, $email, $password){
         $table = $this->getTableName();
         $stmt = $this->pdo->prepare("INSERT INTO `$table` (`username`, `email`, `password`) VALUES (:username, :email, :password);");
         $stmt->execute([
@@ -36,4 +27,31 @@ class UsersRepository extends AbstractRepository
         ]);
         return true;
     }
+
+    public function update(UserModel $model){
+        $table = $this->getTableName();
+        $stmt = $this->pdo->prepare("UPDATE `{$table}` SET `first_name` = :first_name, `last_name` = :last_name,`birthday` = :birthday,`email` = :email,`street` = :street,`housenumber` = :housenumber,`postcode` = :postcode,`city` = :city WHERE `id` = :id;");
+        $stmt->execute([
+            'id' => $model->id,
+            'first_name' => $model->first_name,
+            'last_name' => $model->last_name,
+            'birthday' => $model->birthday,
+            'email' => $model->email,
+            'street' => $model->street,
+            'housenumber' => $model->housenumber,
+            'postcode' => $model->postcode,
+            'city' => $model->city,
+        ]);
+    }
+
+    public function findByUsername($username){
+        $table = $this->getTableName();
+        $model = $this->getModelName();
+        $stmt = $this->pdo->prepare("SELECT * FROM $table WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $model);
+        return $stmt->fetch(PDO::FETCH_CLASS);
+    }
+
+
 }
