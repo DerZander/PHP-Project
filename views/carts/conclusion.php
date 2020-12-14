bd00cfa7ab0e4fb2ec096e922a60f8a341f9ef80
DerZander <timo.zander@outlook.com>
2020-12-14 20:26:31 +0100

    (feat): create thankyou page

---
+++ views/carts/conclusion.php
@@ -0,0 +1,18 @@
+<?php include __DIR__ . "/../layouts/header.php"?>
+<div class="card text-dark bg-light m-3 ">
+    <div class="card-header">
+        <div class="row">
+            <div class="col">
+                <h1 class="m-0">
+                    Vielen Dank für Ihren Einkauf
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Ihre bestellung ist mit beigefügter Rechnung unterwegs zu ihnen.</h5>
+        <p class="card-text">Bei Interesse können Sie alle Produkte bewerten.</p>
+        <a href="products" class="btn btn-primary">zu den Produkten</a>
+    </div>
+</div>
+<?php include __DIR__ . "/../layouts/footer.php"?>
